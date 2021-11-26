3ee12e7f0164a80f86f5c9fe9492dfbb9f84384d
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2021-11-26 10:16:52 -0400

    tracking feature

--- CosmWasm/Current Version (CosmWasm 1.0)/Prescription-Contract/src/msg.rs
+++ CosmWasm/Current Version (CosmWasm 1.0)/Prescription-Contract/src/msg.rs
@@ -11,13 +11,14 @@ pub struct InstantiateMsg {
     pub medication:String,
     pub diagnosis:String,
     pub count:i32,
+    //pub last_access:String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    CreatePrescription {personal_info:String,medication:String,diagnosis:String}
-    //Reset { count: i32 },
+    CreatePrescription {personal_info:String,medication:String,diagnosis:String},
+    Tracking {},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -25,6 +26,7 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
     // GetCount returns the current prescription as a json-encoded number
     GetCount {},
+
 }
 
 // We define a custom struct for each query response
@@ -32,9 +34,10 @@ pub enum QueryMsg {
 pub struct CountResponse {
     pub doctor: Addr,
     pub patient: Addr,
-    pub owner: Addr,
+   // pub owner: Addr,
     pub personal_info: String,
     pub medication:String,
     pub diagnosis:String,
     pub count:i32,
+    pub last_access:Addr,
 }
