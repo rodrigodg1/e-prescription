de6cb925b5971524175db8bfaaef7eeb5e6b6e60
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2021-11-26 10:17:27 -0400

    tracking feature

--- CosmWasm/Current Version (CosmWasm 1.0)/Prescription-Contract/src/state.rs
+++ CosmWasm/Current Version (CosmWasm 1.0)/Prescription-Contract/src/state.rs
@@ -8,11 +8,12 @@ use cw_storage_plus::Item;
 pub struct State {
     pub doctor: Addr,
     pub patient: Addr,
-    pub owner: Addr,
+    //pub owner: Addr, //owner = who creates a instance  = doctor
     pub personal_info:String,
     pub medication:String,
     pub diagnosis:String,
-    pub count:i32,
+    pub count:i32, // count updates in contract (i.e., new prescription)
+    pub last_access: Addr,
 }
 
 pub const STATE: Item<State> = Item::new("state");
