a0ba41072daaa1c95f8eb6611dbf901e0cfb0503
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2021-12-07 15:14:55 -0400

    Update MedicationSalesContract.sol

--- Ethereum/Contracts/Medication-Sales-Contract/MedicationSalesContract.sol
+++ Ethereum/Contracts/Medication-Sales-Contract/MedicationSalesContract.sol
@@ -1,35 +1,39 @@
 pragma solidity ^0.5.11;
 
 
-contract Report{
-    address originAddr;
-    address regulatorAddr;
-    string  data;
-    uint256  count;
+contract Sales{
+    address pharmacyAddr; //instance owner
+    address patientAddr;
+    string medication;
+    string dosage;
+    uint256 amount=0;
+    address last_update_addr;
 
 
-    //report contract constructor (relation with origin and regulator)
-    constructor(address _regulator) public {
-        originAddr = msg.sender;
-        regulatorAddr = _regulator;
+    //sales contract constructor (relation with pharmacy and patient)
+    constructor(address _patient) public {
+        pharmacyAddr = msg.sender;
+        last_update_addr = msg.sender;
+        patientAddr = _patient;
     }
 
-    //only the instance owner can create report transactions
-    function setReport(string memory _data) public {
-        if (msg.sender == originAddr){
-            data = _data;
-            count = count + 1;
+    //only the instance owner can create supply transactions
+    function sell_medication(string memory _medication, string memory _dosage, uint256  _amount) public {
+        if (msg.sender == pharmacyAddr){
+            medication = _medication;
+            dosage = _dosage;
+            amount = _amount;
+            last_update_addr = msg.sender;
+
         }else{
-            require(msg.sender == originAddr,"Sender not authorized.");
+            require(msg.sender == pharmacyAddr,"Sender not authorized.");
         }
     
     }
 
-
-
     //return regulator addres and report data
-    function get_report_info() public view returns (address, string memory){
-        return (regulatorAddr,data);
+    function get_sales_info() public view returns (address,address, string memory,string memory, uint256,address){
+        return (pharmacyAddr,patientAddr, medication,dosage, amount,last_update_addr);
     }
 
 
