f35fd046d95df586e66af56cfc064a1d96944ff5
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2021-12-08 07:52:39 -0400

    Create RewardContract.sol

---
+++ Ethereum/Contracts/Reward-Contract/RewardContract.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.11;
+
+// contract for regulator to send ether to patient in case of proof of fraud.
+
+contract Reward {
+
+    address regulator;
+
+
+    modifier onlyOwner() {
+        require(msg.sender == regulator, "Only regulator can call this method");
+        _;
+    }
+
+    constructor() public{
+        regulator = msg.sender;
+    }
+
+    function sendBal(address payable receiver) payable external onlyOwner {
+        uint256 amount = msg.value;
+        receiver.transfer(amount);  
+    }
+}
